d32dfbde2a03e3e5b41b2ff09128db053a6b881d
Prathyusha-mind <64723764+Prathyusha-mind@users.noreply.github.com>
2020-05-03 13:03:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capabality1
+{
+    class Program
+    {
+        int age;
+        string name;
+        public Program(int age,string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+        public Program()
+        {
+
+        }
+        
+        static void Main(string[] args)
+        {
+            int Sum = PrintSum(1, 2, 3);
+            Console.WriteLine(Sum);
+            Console.ReadLine();
+        }
+        public static int PrintSum(int n1, int n2, int n3)
+        {
+            int sum = n1 + n2 + n3;
+            return sum;
+        }
+    }
+}
+
+
+
+
+
+
